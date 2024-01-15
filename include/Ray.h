8398bc882b345bb94701ce5985d02ff45d4e4a65
= <=>
2024-01-15 00:19:06 -0300

    feat: ray

---
+++ include/Ray.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Vector3D.h"
+
+class Ray {
+ public:
+  Ray(void) = default;
+
+  Ray(const Vector3D&, const Vector3D&);
+
+  Vector3D origin(void) const;
+
+  Vector3D direction(void) const;
+
+  Vector3D position(const float) const;
+
+ private:
+  Vector3D _origin, _direction;
+};
---
+++ src/Ray.cpp
@@ -0,0 +1,10 @@
+#include "Ray.h"
+
+Ray::Ray(const Vector3D& origin, const Vector3D& direction)
+    : _origin(origin), _direction(direction) {}
+
+Vector3D Ray::origin(void) const { return _origin; }
+
+Vector3D Ray::direction(void) const { return _direction; }
+
+Vector3D Ray::position(const float k) const { return _origin + _direction * k; }
