b0eae0d05537a5a1f0b36d5099421e4df9a1d2ab
AssoDePicche <samuelprado730@gmail.com>
2025-06-07 18:15:34 -0300

    feat: read settings.json file

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,27 @@
 #include <cstdint>
+#include <filesystem>
 #include <format>
+#include <fstream>
 #include <iostream>
+#include <nlohmann/json.hpp>
+#include <string>
 
 auto main(void) -> int {
-  const auto aspect_ratio = 16.0f / 9.0f;
+  const std::string filename = "settings.json";
 
-  const auto image_width = 256u;
+  if (!std::filesystem::exists(filename)) {
+    std::cerr << std::format("{} not found\n", filename);
+  }
 
-  int image_height = image_width / aspect_ratio;
+  std::ifstream stream(filename);
+
+  nlohmann::json json = nlohmann::json::parse(stream);
+
+  const float aspect_ratio = json["aspect-ratio"];
+
+  const uint64_t image_width = json["image-width"];
+
+  uint64_t image_height = image_width / aspect_ratio;
 
   if (image_height < 1) {
     image_height = 1;
@@ -15,13 +29,13 @@ auto main(void) -> int {
 
   std::cout << std::format("P3\n{} {}\n255\n", image_width, image_height);
 
-  for (uint16_t i = 0u; i < image_width; ++i) {
-    for (uint16_t j = 0; j < image_height; ++j) {
-      const int red = 255.999f * double(j) / (image_width - 1);
+  for (uint64_t i = 0u; i < image_width; ++i) {
+    for (uint64_t j = 0; j < image_height; ++j) {
+      const uint16_t red = 255.999f * double(j) / (image_width - 1);
 
-      const int green = 255.999f * double(i) / (image_height - 1);
+      const uint16_t green = 255.999f * double(i) / (image_height - 1);
 
-      const int blue = 0;
+      const uint16_t blue = 0;
 
       std::cout << std::format("{} {} {}\n", red, green, blue);
     }
